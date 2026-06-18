70d53f563f8177210a08d258ac17cc0149dc7dfc
yabelinda <yabelinda@gmail.com>
2026-06-18 20:56:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ menuBtn?.addEventListener('click', () => {
 document.querySelectorAll('.nav a').forEach(link => {
   link.addEventListener('click', () => nav.classList.remove('open'));
 });
+
+let lastScroll = 0;
+const topbar = document.querySelector(".topbar");
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll > lastScroll && currentScroll > 80) {
+    topbar.classList.add("hide");
+  } else {
+    topbar.classList.remove("hide");
+  }
+
+  lastScroll = currentScroll;
+});
